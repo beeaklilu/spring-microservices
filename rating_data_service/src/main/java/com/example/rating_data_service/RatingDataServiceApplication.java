967c3bbc4b680a2d3db63531176b520a21fa9a1e
beeAk <se.blen.aklilu@gmail.com>
2021-08-12 11:06:15 -0500

    register rating data service as a eureka client

--- rating_data_service/src/main/java/com/example/rating_data_service/RatingDataServiceApplication.java
+++ rating_data_service/src/main/java/com/example/rating_data_service/RatingDataServiceApplication.java
@@ -2,9 +2,11 @@ package com.example.rating_data_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RatingDataServiceApplication {
 
     public static void main(String[] args) {
--- rating_data_service/src/main/java/com/example/rating_data_service/RatingsSource.java
+++ rating_data_service/src/main/java/com/example/rating_data_service/RatingsSource.java
@@ -1,8 +1,12 @@
 package com.example.rating_data_service;
 
+import org.apache.catalina.User;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -11,5 +15,13 @@ public class RatingsSource {
     @RequestMapping("/{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId){
         return new Rating(movieId, 3);
+    }
+
+    @RequestMapping("users/{userId}")
+    public UserRating getRatingByUser(@PathVariable("userId") String userId){
+        List<Rating> ratings = Arrays.asList(new Rating("1234", 4));
+        UserRating userRatings = new UserRating();
+        userRatings.setUserRating(ratings);
+        return userRatings;
     }
 }
