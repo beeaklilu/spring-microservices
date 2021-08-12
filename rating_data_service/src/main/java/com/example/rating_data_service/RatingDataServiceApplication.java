befd46528613024642bccae4f1f3164d4de8bbf1
beeAk <se.blen.aklilu@gmail.com>
2021-08-11 21:50:20 -0500

    rating data service

--- rating_data_service/src/main/java/com/example/rating_data_service/RatingDataServiceApplication.java
+++ rating_data_service/src/main/java/com/example/rating_data_service/RatingDataServiceApplication.java
@@ -3,6 +3,7 @@ package com.example.rating_data_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class RatingDataServiceApplication {
 
